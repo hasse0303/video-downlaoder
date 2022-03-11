c13ca9fca0d4e955af2eed23682dec4531fefe30
Has Se <has.se@ecoinsoft.com>
2022-03-11 13:09:54 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { VideoDownloaderService } from './video-downloader.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form!: FormGroup;
   title = 'video-downloader';
+  videoData: any;
+  constructor(
+    private vdoService: VideoDownloaderService,
+    private fb: FormBuilder
+  ) {
+
+  }
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      linkVideo: ['']
+    })
+  }
+  search() {
+    const link = this.form.get('linkVideo')?.value
+    this.vdoService.getVdo(link).subscribe(data => {
+     this.videoData = data;
+     console.log(this.videoData);
+
+    })
+  }
 }
